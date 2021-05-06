cb407278698e929ba4adffaf9078700679479c7b
sp <reza.ghasemi8@gmail.com>
2021-05-06 17:55:33 +0430

    WebinarPlatform AbstractFactory

---
+++ AbstractFactory/WebinarPlatformAbstractFactory.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.AbstractFactory
+{
+
+    interface IWebinarPlatform
+    {
+        public IJoinWebinar JoinWebinar();
+        public IStartAsAttendar StartAsAttender();
+        public IStartAsManager StartAsManager();
+        public IGetRecords GetRecords();
+    }
+
+
+
+    class BigBlueButton : IWebinarPlatform
+    {
+        public IJoinWebinar JoinWebinar()
+        {
+            return new BigBlueButtonJoinWebinar();
+        }
+
+        public IStartAsAttendar StartAsAttender()
+        {
+            return new BigBlueButtonStartAsAttendar();
+        }
+
+        public IStartAsManager StartAsManager()
+        {
+            return new BigBlueButtonStartAsManager();
+        }
+
+        public IGetRecords GetRecords()
+        {
+            return new BigBlueButtonGetRecords();
+        }
+    }
+    class SkyRoom : IWebinarPlatform
+    {
+        public IJoinWebinar JoinWebinar()
+        {
+            return new SkyRoomJoinWebinar();
+        }
+
+        public IStartAsAttendar StartAsAttender()
+        {
+            return new SkyRoomStartAsAttendar();
+        }
+
+        public IStartAsManager StartAsManager()
+        {
+            return new SkyRoomStartAsManager();
+        }
+
+        public IGetRecords GetRecords()
+        {
+            return new SkyRoomGetRecords();
+        }
+    }
+
+    interface IJoinWebinar
+    {
+        string Execute();
+    }
+    interface IStartAsManager
+    {
+        string Execute();
+    }
+    interface IStartAsAttendar
+    {
+        string Execute();
+    }
+    interface IGetRecords
+    {
+        string Execute();
+    }
+
+
+    class BigBlueButtonJoinWebinar : IJoinWebinar
+    {
+        public string Execute()
+        {
+            return "BigBlueButtonJoinWebinar";
+        }
+    }
+    class SkyRoomJoinWebinar : IJoinWebinar
+    {
+        public string Execute()
+        {
+            return "SkyRoomJoinWebinar";
+        }
+    }
+
+    class BigBlueButtonStartAsManager : IStartAsManager
+    {
+        public string Execute()
+        {
+            return "BigBlueButtonStartAsManager";
+        }
+    }
+    class SkyRoomStartAsManager : IStartAsManager
+    {
+        public string Execute()
+        {
+            return "SkyRoomStartAsManager";
+        }
+    }
+
+    class SkyRoomStartAsAttendar : IStartAsAttendar
+    {
+        public string Execute()
+        {
+            return "SkyRoomStartAsAttendar";
+        }
+    }
+    class BigBlueButtonStartAsAttendar : IStartAsAttendar
+    {
+        public string Execute()
+        {
+            return "BigBlueButtonStartAsAttendar";
+        }
+    }
+
+    class BigBlueButtonGetRecords : IGetRecords
+    {
+        public string Execute()
+        {
+            return "BigBlueButtonGetRecords";
+        }
+    }
+    class SkyRoomGetRecords : IGetRecords
+    {
+        public string Execute()
+        {
+            return "SkyRoomGetRecords";
+        }
+    }
+
+    class WebinarPlatformClient
+    {
+        private IJoinWebinar joinWebinar;
+        private IStartAsManager startAsManager;
+        private IStartAsAttendar startAsAttendar;
+        private IGetRecords getRecords;
+
+        public WebinarPlatformClient(IWebinarPlatform factory)
+        {
+            joinWebinar = factory.JoinWebinar();
+            startAsManager = factory.StartAsManager();
+            startAsAttendar = factory.StartAsAttender();
+            getRecords = factory.GetRecords();
+        }
+
+        public string JoinWebinar()
+        {
+            return joinWebinar.Execute();
+        }
+        public string StartAsManager()
+        {
+            return startAsManager.Execute();
+        }
+        public string StartAsAttender()
+        {
+            return startAsAttendar.Execute();
+        }
+        public string GetRecords()
+        {
+            return getRecords.Execute();
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,29 +9,45 @@ namespace DesignPatterns
         {
             #region  Absstract Factory
 
-            /// <summary>  
-            /// Abstract Factory Pattern Demo  
-            /// </summary>  
+            #region FirstSample
+            //IMobilePhone nokiaMobilePhone = new Nokia();
+            //MobileClient nokiaClient = new MobileClient(nokiaMobilePhone);
 
-            static void Main()
-            {
-                IMobilePhone nokiaMobilePhone = new Nokia();
-                MobileClient nokiaClient = new MobileClient(nokiaMobilePhone);
+            //Console.WriteLine("********* NOKIA **********");
+            //Console.WriteLine(nokiaClient.GetSmartPhoneModelDetails());
+            //Console.WriteLine(nokiaClient.GetNormalPhoneModelDetails());
 
-                Console.WriteLine("********* NOKIA **********");
-                Console.WriteLine(nokiaClient.GetSmartPhoneModelDetails());
-                Console.WriteLine(nokiaClient.GetNormalPhoneModelDetails());
+            //IMobilePhone samsungMobilePhone = new Samsung();
+            //MobileClient samsungClient = new MobileClient(samsungMobilePhone);
 
-                IMobilePhone samsungMobilePhone = new Samsung();
-                MobileClient samsungClient = new MobileClient(samsungMobilePhone);
+            //Console.WriteLine("******* SAMSUNG **********");
+            //Console.WriteLine(samsungClient.GetSmartPhoneModelDetails());
+            //Console.WriteLine(samsungClient.GetNormalPhoneModelDetails());
 
-                Console.WriteLine("******* SAMSUNG **********");
-                Console.WriteLine(samsungClient.GetSmartPhoneModelDetails());
-                Console.WriteLine(samsungClient.GetNormalPhoneModelDetails());
 
-                Console.ReadKey();
-            }
 
+            #endregion
+
+            #region SecondSample WebinarPlatform
+            IWebinarPlatform bbbPlatform = new BigBlueButton();
+            WebinarPlatformClient bbbClient = new WebinarPlatformClient(bbbPlatform);
+
+            Console.WriteLine("********* bbb **********");
+            Console.WriteLine(bbbClient.JoinWebinar());
+            Console.WriteLine(bbbClient.StartAsAttender());
+            Console.WriteLine(bbbClient.StartAsManager());
+            Console.WriteLine(bbbClient.GetRecords());
+
+            IWebinarPlatform skyRoomPlatform = new SkyRoom();
+            WebinarPlatformClient skyRoomClient = new WebinarPlatformClient(skyRoomPlatform);
+
+            Console.WriteLine("******* sky room **********");
+            Console.WriteLine(skyRoomClient.JoinWebinar());
+            Console.WriteLine(skyRoomClient.StartAsAttender());
+            Console.WriteLine(skyRoomClient.StartAsManager());
+            Console.WriteLine(skyRoomClient.GetRecords());
+
+            #endregion
 
             Console.ReadKey();
             #endregion
