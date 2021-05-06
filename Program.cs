86befcacff47a256f11be70f30c65bfc8b5cbcac
sp <reza.ghasemi8@gmail.com>
2021-05-06 16:54:45 +0430

    AbstractFactory Pattern

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using DesignPatterns.AbstractFactory;
 
 namespace DesignPatterns
 {
@@ -6,7 +7,24 @@ namespace DesignPatterns
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!22");
+            #region  Absstract Factory
+
+            // Abstract factory #1
+
+            AbstractFactory.AbstractFactory factory1 = new ConcreteFactory1();
+            Client client1 = new Client(factory1);
+            client1.Run();
+
+            // Abstract factory #2
+
+            AbstractFactory.AbstractFactory factory2 = new ConcreteFactory2();
+            Client client2 = new Client(factory2);
+            client2.Run();
+
+            // Wait for user input
+
+            Console.ReadKey();
+            #endregion
         }
     }
 }
